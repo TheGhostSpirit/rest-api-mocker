7fe06339666d62a41526b7a1b5196de2dc59435a
Baptiste <baptiste.lego1@gmail.com>
2020-05-19 17:16:04 +0200

    setup angular app

---
+++ model-manager/src/app/model-import/model-import.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-model-import',
+  templateUrl: './model-import.component.html',
+  styleUrls: ['./model-import.component.less']
+})
+export class ModelImportComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
